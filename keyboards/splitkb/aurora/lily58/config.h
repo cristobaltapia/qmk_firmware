ee180451ec7d3f09178adbdc6705bce9ef4186b5
Cristóbal Tapia Camú <crtapia@gmail.com>
2025-12-09 14:08:16 +0100

    add configuration for Lily58

--- keyboards/splitkb/aurora/lily58/config.h
+++ keyboards/splitkb/aurora/lily58/config.h
@@ -16,6 +16,10 @@
 
 #pragma once
 
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
+#define SPLIT_WATCHDOG_ENABLE
+
 // Not yet available in `info.json`
 #ifdef OLED_ENABLE
 #   define OLED_FONT_H "keyboards/splitkb/aurora/lily58/glcdfont.c"
